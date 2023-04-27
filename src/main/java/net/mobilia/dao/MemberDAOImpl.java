6392799668d850b527c12c8721c14ed237320baa
HongSeoungUk <sungk97@naver.com>
2023-04-27 16:50:34 +0900

    회원가입 뜨게 http://localhost:8111/member_join

--- src/main/java/net/mobilia/dao/MemberDAOImpl.java
+++ src/main/java/net/mobilia/dao/MemberDAOImpl.java
@@ -1,5 +1,12 @@
 package net.mobilia.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class MemberDAOImpl implements MemberDAO {
 
+	@Autowired
+	private SqlSession sqlSession;
 }
