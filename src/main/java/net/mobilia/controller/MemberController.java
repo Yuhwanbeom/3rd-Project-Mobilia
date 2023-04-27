2e81cb8256f2638f2a924d633b452a7f88d005a0
1101- <1101-@DESKTOP-FVNGG44>
2023-04-27 17:00:28 +0900

    d

--- src/main/java/net/mobilia/controller/MemberController.java
+++ src/main/java/net/mobilia/controller/MemberController.java
@@ -1,12 +1,12 @@
 package net.mobilia.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MemberController {
 
-	int a=10;
-	int b=20;
-	String id="홍길동";
+	
+	
 
-
-	//이것은 유환범 작성
 
 }
